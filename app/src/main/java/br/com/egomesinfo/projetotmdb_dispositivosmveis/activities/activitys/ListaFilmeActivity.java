7e46978c9d06850e5a6ba2a2f5e50c79890d4f02
Augusto Hideki Cioccia Kobayashi <djhideki@djhideki.com>
2018-11-20 11:59:25 -0200

    agora vai

---
+++ app/src/main/java/br/com/egomesinfo/projetotmdb_dispositivosmveis/activities/activitys/ListaFilmeActivity.java
@@ -0,0 +1,21 @@
+package br.com.egomesinfo.projetotmdb_dispositivosmveis.activities.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+import br.com.egomesinfo.projetotmdb_dispositivosmveis.R;
+import br.com.egomesinfo.projetotmdb_dispositivosmveis.activities.adapters.ListMoviesAdapter;
+import br.com.egomesinfo.projetotmdb_dispositivosmveis.activities.adapters.ListMoviesAdapter;
+
+public class ListaFilmeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

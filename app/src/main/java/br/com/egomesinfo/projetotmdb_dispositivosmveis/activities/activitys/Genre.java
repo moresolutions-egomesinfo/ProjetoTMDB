1f5703bf0216200c525d8c49d863ad8874c03d7d
felipe <felipeconceicao59@gmail.com>
2018-11-23 22:54:57 -0200

    Criadas as Classes Genre e Movies
    Adicionadas as Strings com as url's de request no arquivo setting
    
    renomeada as classes de adapter

---
+++ app/src/main/java/br/com/egomesinfo/projetotmdb_dispositivosmveis/activities/activitys/Genre.java
@@ -0,0 +1,13 @@
+package br.com.egomesinfo.projetotmdb_dispositivosmveis.activities.activitys;
+
+public class Genre {
+
+    public final String nameGenre;
+    public final int idGenre ;
+
+    public Genre (int idGenre, String nameGenre){
+        this.idGenre=idGenre;
+        this.nameGenre = nameGenre;
+    }
+}
+
